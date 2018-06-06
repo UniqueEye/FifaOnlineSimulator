512400a110fa991d728b2ea6ecdd7d8f5bd16e09
Yuan <Yuan@DESKTOP-63J1RKM>
2018-06-06 22:42:09 +0900

    Person - Prototype

--- FifaOnlineSimulator/src/user/Person.java
+++ FifaOnlineSimulator/src/user/Person.java
@@ -8,7 +8,9 @@ public class Person {
 	private String name;
 	private int team;
 	private int balance;
-	ArrayList<Player> list = new ArrayList<>();
+	ArrayList<Player> players = new ArrayList<>();
+	Player[] starting = new Player[11];
+	HashMap<Pack, Integer> items = new HashMap<>();
 	
 	public Person(String name) {
 		this.name = name;
@@ -18,7 +20,15 @@ public class Person {
 		return name;
 	}
 	
+	public void addBalance(int amount) {
+		balance += amount;
+	}
+	
+	public void subBalance(int amount) {
+		balance -= amount;
+	}
+	
 	void addPlayer(Player player) {
-		list.add(player);
+		players.add(player);
 	}
 }
