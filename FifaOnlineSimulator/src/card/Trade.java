2f112cbc61514f6c4f7a627133ae0bd15bcfece8
mook <solomon2109@naver.com>
2018-06-12 22:45:34 +0900

    editted
    Signed-off-by: mook <solomon2109@naver.com>

--- FifaOnlineSimulator/src/card/Trade.java
+++ FifaOnlineSimulator/src/card/Trade.java
@@ -1,14 +1,20 @@
 package card;
 
+import java.util.ArrayList;
 import java.util.Random;
+
+import db.SearchOVR;
 
 public class Trade {
 	private int avg;
+	private Player result;
+	private SearchOVR search;
 	
-	Trade(Player p1, Player p2, Player p3, Player p4, Player p5, Player p6, Player p7, Player p8, Player p9, Player p10) {
+	public Player getResult() {return this.result;}
+	public Trade(Player p1, Player p2, Player p3, Player p4, Player p5, Player p6, Player p7, Player p8, Player p9, Player p10) {
 		avg = (p1.getOvr() + p2.getOvr() + p3.getOvr() + p4.getOvr() + p5.getOvr() + p6.getOvr() + p7.getOvr() + p8.getOvr() + p9.getOvr() + p10.getOvr()) / 10;
 		
-		p1 = proceed(avg);
+		result = proceed(avg);
 	}
 	
 	Player proceed(int avg) {
@@ -19,13 +25,32 @@ public class Trade {
 		int num = rand.nextInt(100);
 		
 		if (num < 20) {
-			p1 = new Player(id);
+			if (avg+4>94) {
+				search = new SearchOVR(94, 100);
+			}
+			else 
+				search = new SearchOVR(avg+4,100);
+			
+			int ranNum = rand.nextInt(search.getList().size());
+			
+			p1 = new Player(search.getList().get(ranNum));
 		}
 		else if (num < 50) {
-			p1 = new Player(id);
+			if (avg-4<46) {
+				search = new SearchOVR(0,46);
+			}
+			else {
+				search = new SearchOVR(0,avg-4);
+			}
+			int ranNum = rand.nextInt(search.getList().size());
+			
+			p1 = new Player(search.getList().get(ranNum));
 		}
 		else {
-			p1 = new Player(id);
+			search = new SearchOVR(avg-4,avg+4);
+			int ranNum = rand.nextInt(search.getList().size());
+			
+			p1 = new Player(search.getList().get(ranNum));
 		}
 		
 		return p1;
